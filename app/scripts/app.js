5c298e433ced00bd28f3b35fa77a48864c1a5470
mishrole <mitrodle@gmail.com>
2017-09-19 22:10:28 -0500

    Actualizando | Clase Martes 19

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,17 @@
 const mostrarCiudades = ciudades => {
-  ciudades.forEach(ciudad => console.log(ciudad.nombre));
+	const listadoCiudades = [];
+
+	ciudades.forEach(ciudad => 
+	  	const htmlCiudad = `<h4>${ciudad.nombre}</h4>
+	  						<p data-id="2147714"><span>20.5</span>°<img src=""></p>
+	  						<p>${ciudad.descripcion}</p>`;
+	  	listadoCiudades.push(htmlCiudad);
+	  	// console.log(ciudad.nombre));
+	});
+
+	const contenedorCiudades = document.querySelector('.row.marketing');
+	contenedorCiudades.innerHTML = listadoCiudades.join('');
+
 };
 
 export default mostrarCiudades;
